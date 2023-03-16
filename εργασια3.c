50696cc7cc13e0a1a90d4c9fd2bf070cd51d66a3
Null <80452309+faniskantzaris@users.noreply.github.com>
2023-03-16 13:10:46 +0200

    Update εργασια3.c

--- εργασια3.c
+++ εργασια3.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #define SIZE 2
 
-
-struct student
+struct student //Defining the student struct
 {
-    int am;
-    int grades[5];
-    char name;
-    double average[10];
+    int am; //Identification number
+    int grades[5]; //Grades array
+    char name; //Student name
+    double average[10]; //The grades average
 };
 
 int main()
 {
     int i,j,min_el,min_pos,sum,temp;
     struct student pers_arr[SIZE];
-    for (i = 0; i<SIZE; i++)
+    for (i = 0; i<SIZE; i++) //Inserting the informations
     {
         printf("Give the name and press enter: ");
         scanf("%s", &pers_arr[i].name);
-        printf("Give the am and press enter: ");
+        printf("Give the identification number and press enter: ");
         scanf("%d", &pers_arr[i].am);
         printf("Give the 5 grades: ");
           for (j = 0; j<5; j++)
@@ -30,7 +29,7 @@ int main()
     }
     fflush(stdin);
 
-    for (i = 0; i<SIZE; i++)
+    for (i = 0; i<SIZE; i++) //Calculating the average of the grades
     {
         min_pos = i;
 
@@ -49,7 +48,7 @@ int main()
         }
     }
 
-    printf("|Name  |Average|");
+    printf("|Name  |Average|"); //Printing the information sorted by discending
     for (i = 0; i<SIZE; i++)
     {
         printf("|%s  |%f|" ,pers_arr[i].name,pers_arr[i].average[i]);
